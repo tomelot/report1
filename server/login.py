0cf003798e96ef4eac12069075249ea69ba81567
tomlot <tomlot2012@gmail.com>
2020-11-15 23:18:40 +0200

    added basic loging system (api)

---
+++ server/login.py
@@ -0,0 +1,34 @@
+import json
+import hashlib
+
+USERS_FILE = "users.json"
+global_users = {}
+
+def login(user, password):
+    if not user in global_users:
+        return None
+    cipher_password = hashlib.sha3_512()
+    cipher_password.update(password)
+    if global_users[user][0] == cipher_password.hexdigest():
+        update_file()
+        return global_users[user][1]
+    return None
+
+def register(user, password, cookie):
+    if user in global_users:
+        return False
+    cipher_password = hashlib.sha3_512()
+    cipher_password.update(password)
+    global_users[user] = [cipher_password.hexdigest(), cookie]
+    update_file()
+    return True
+
+def change_cookie(user, cookie):
+    if not user in global_users:
+        return False
+    global_users[user][1] = cookie
+    update_file()
+    return True
+
+def update_file():
+    open(USERS_FILE, "w").write(json.dumps(global_users))
